636ff4805cc541bc1a9e350bc91b900344bc3a12
tleoutline <tleoutline@gmail.com>
2025-03-04 18:44:09 +0700

    chore(zsh/fzf): remove yay commands from fzf

--- dot_config/zsh/custom/fzf/fzf.plugin.zsh
+++ dot_config/zsh/custom/fzf/fzf.plugin.zsh
@@ -35,19 +35,6 @@ fv() {
   fi
 }
 
-# Install packages using yay (change to pacman/AUR helper of your choice)
-yayin() {
-    if [ $1 ]; then
-        yay -Ssq "$1" 2>/dev/null | fzf -m --preview 'yay -Si {1}' --preview-window 'right,70%' | xargs -ro yay -S
-    else
-        yay -Slq 2>/dev/null | fzf -m --preview 'yay -Si {1}' --preview-window 'right,70%' | xargs -ro yay -S
-    fi
-}
-# Remove installed packages (change to pacman/AUR helper of your choice)
-yayrm() {
-    yay -Qq | fzf -q "$1" -m --preview 'yay -Qi {1}' --preview-window 'right,70%' | xargs -ro yay -Rns
-}
-
 # fzf picker for zoxide under curent dir
  
 fz() {
@@ -86,7 +73,7 @@ function _fz_jump_paste_command {
     zle reset-prompt > /dev/null 2>&1
 }
 
-fzp() {
+function fzp {
   local result
   local fzf_args=(
     --height 40%
